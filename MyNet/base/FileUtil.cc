6cd96b2a8d40e2041b6bf99be4a7d3f471ea5047
root <root@localhost.localdomain>
2014-09-23 04:34:43 -0700

    Add FileUtil

---
+++ MyNet/base/FileUtil.cc
@@ -0,0 +1,202 @@
+#include "FileUtil.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+
+namespace MyNet
+{
+namespace base
+{
+    ReadSmallFile::ReadSmallFile(const std::string& filename)
+        : m_filename(filename),
+          m_fd(::open(filename.c_str(), O_RDONLY | O_CLOEXEC)),
+          m_err(0),
+          m_size(0)
+    {
+        m_buff[0] = '\0';
+        if(m_fd < 0)
+            m_err = errno;
+    }
+
+    ReadSmallFile::~ReadSmallFile()
+    {
+        int ret;
+        if(m_fd >= 0)
+        {
+           do
+           {
+               // should check EINTR
+               ret = ::close(m_fd);
+           }while((ret == -1) && (errno == EINTR) );
+
+        }
+    }
+
+    int ReadSmallFile::readToBuffer(int *size)
+    {
+        int err = m_err;
+        if(m_fd >= 0)
+        {
+            ssize_t ret = 0;
+            do
+            {
+                ret = ::pread(m_fd, m_buff, sizeof(m_buff) - 1, 0);
+            } while((ret == -1) && (errno = EINTR));
+            if(ret >= 0)
+            {
+                m_buff[ret] = '\0';
+                if(size)
+                {
+                    *size = static_cast<int> (n);
+                }
+            }
+            else
+            {
+                err = errno;
+            }
+        }
+        return err;
+    }
+
+    int ReadSmallFile::readToString(std::string& data, int64_t maxSize,
+                                    int64_t* fileSize, int64_t* modifyTime,
+                                    int64_t* createTime)
+    {
+        assert(sizeof(off_t) == 8);
+        int err = m_err;
+        int64_t readSize = 0;
+        if(m_fd >= 0)
+        {
+            struct stat fileStat;
+            int ret = ::fstat(m_fd, &fileStat);
+            if(ret == 0)
+            {
+                //success
+                if(S_ISREG(fileStat.st_mode) && fileSize)
+                {
+                    *fileSize = fileStat.st_size;
+                    readSize = std::min(maxSize, fileStat.st_size);
+                    data.reserve(readSize);
+                }
+                else if(S_ISDIR(fileStat.st_mode))
+                {
+                    err = EISDIR;
+                }
+                if(modifyTime)
+                {
+                    *modifyTime = fileStat.st_mtime;
+                }
+                if(createTime)
+                {
+                    *createTime = fileStat.st_ctime;
+                }
+            }
+            else
+            {
+                err = errno;
+            }
+        }
+        int64_t readed = 0;
+
+        if(readSize > 0)
+        {
+            while(readed < readSize)
+            {
+                ssize_t n = ::read(m_fd, m_buff, sizeof(m_buff));
+                if(n > 0)
+                {
+                    readed += n;
+                    data.append(m_buff, n);
+                }
+                else if(n == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    if(errno != EINTR)
+                    {
+                        err = errno;
+                        break;
+                    }
+                }
+            }
+        }
+        return err;
+    }
+
+
+    WriteFile::WriteFile(const std::string& filename)
+        : m_filename(filename),
+          m_file(::fopen(filename.c_str(), "ae")),
+          m_nBytesWritten(0)
+          m_valid(true)
+    {
+        if(m_file == NULL)
+            m_valid = false;
+        else
+        {
+            ::setbuffer(m_file, m_buff, sizeof(m_buff));
+        }
+    }
+
+    WriteFile::~WriteFile()
+    {
+        if(m_valid)
+        {
+            ::fclose(m_file);
+        }
+    }
+
+    WriteFile::flush()
+    {
+        if(m_valid)
+        {
+            ::flush(m_file);
+        }
+    }
+    size_t WriteFile::writeLine(const std::string& line)
+    {
+        return writeLine(line.c_str(), line.size());
+    }
+
+    size_t WriteFile::writeLine(const char* line, size_t n)
+    {
+        size_t ret = 0;
+        size_t remain = n;
+        size_t startPos = 0;
+        int err = m_err;
+        if(m_valid)
+        {
+            do
+            {
+                ret = write(line + startPos, remain);
+                if(ret == 0)
+                {
+                    err = ::ferror(m_file);
+                    if(err)
+                    {
+                        // error occured
+                        ::fprintf(stderr, "WriteFile error: %s\n", ::strerror(err));
+                        break;
+                    }
+                }
+                remain = remain - ret;
+                startPos += ret;
+            } while(remain > 0);
+            m_nBytesWritten += n;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    size_t WriteFile::write(const char* line, size_t n)
+    {
+        return ::fwrite_unlocked(line, n, 1, m_file);
+    }
+}
+}
--- MyNet/base/FileUtil.h
+++ MyNet/base/FileUtil.h
@@ -1,6 +1,54 @@
 #ifndef _BASE_FILE_UTIL_H_
 #define _BASE_FILE_UTIL_H_
+#include "NonCopyable.h"
+#include <string.h>
+#include <stdio.h>
+namespace MyNet
+{
+namespace base
+{
+    class ReadSmallFile : private MyNet::Noncopyable
+    {
+        public:
+            ReadSmallFile(const std::string& filename);
+            ~ReadSmallFile();
+            int readToBuffer(int *size);
+            int readToString(std::string& data, int64_t maxSize, int64_t *fileSize,
+                             int64_t *modifyTime, int64_t* createTime);
+            const char* buffer() {return m_buff;}
+        public:
+            const static int kBUFF_SIZE = 64 * 1024;
+        private:
+            std::string m_filename;
+            int m_fd;
+            int m_err;
+            size_t m_size;
+            char m_buff[kBUFF_SIZE];
 
+    };
 
+    class WriteFile: private MyNet::Noncopyable
+    {
+        public:
+            WriteFile(const std::string& filename);
+            ~WriteFile();
+            size_t writeLine(const std::string& line);
+            size_t writeLine(const char* line, size_t n);
+            size_t size() const {return m_nBytesWritten;}
+            bool valid() const { return m_valid;}
+            void flush();
+        private:
+            size_t write(const char* line, size_t n);
+        public:
+            const static kBUFF_SIZE = 64 * 1024;
+        private:
+            std::string m_filename;
+            FILE* m_file;
+            size_t m_nBytesWritten;
+            bool m_valid;
+            char m_buff[kBUFF_SIZE];
+    }
+}
+}
 
 endif
